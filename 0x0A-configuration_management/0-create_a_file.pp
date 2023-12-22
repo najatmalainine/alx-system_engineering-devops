d0d09ac3b15f2575cf9d77bf336ce1bda97e975b
Najat <craazylolla@gmail.com>
2023-12-22 13:30:48 -0800

    m

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # Using puppet, create a file with below specifications
 
 file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school'
+  ensure  => 'present',
   content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
